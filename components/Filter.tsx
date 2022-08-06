61a9fabf1de7f6ebac04ab30a2ce2bc9e4c41a5e
Welber <wlbernardino@gmail.com>
2022-08-06 15:57:30 -0300

    Aula 06/08/2022

---
+++ components/Filter.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <div className="container-filtros">
+            <div className="title">
+                <span>Tarefas</span>
+                <img src='/filter.svg' alt="Filtrar Tarefas" />
+                <div className="form">
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
     return (
         <div className="container-footer">
-            <button><img src='/add.svg' alt='Adicionar tarefas' /> Adicionar Tarefas</button>
+            <button><img src='/add.svg' alt='Adicionar tarefas' />Adicionar Tarefas</button>
             <span>© Copyright {new Date().getFullYear()}. Todos os direitos reservados.</span>
         </div>
     );
