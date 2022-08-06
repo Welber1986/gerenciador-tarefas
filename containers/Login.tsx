e974c1e998a17ab79e28613bdec9bf83b45bcbc0
Welber <wlbernardino@gmail.com>
2022-08-06 10:29:11 -0300

    Aula 06/08/2022

--- containers/Login.tsx
+++ containers/Login.tsx
@@ -1,8 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React, {useState} from "react";
 
 export const Login = () => 
 {
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+
+    const doLogin = () => {
+        try {
+            alert('login:' + login + ', senha:' + password);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div className="container-login">
             <img className="logo" src="/logo.svg" alt="Logo FIAP"  />
@@ -10,13 +22,19 @@ export const Login = () =>
                 <div>
                     <div className="input">
                         <img src="/mail.svg" alt="Informe seu E-mail" />
-                        <input type="text" placeholder="Login" />
+                        <input type="text" placeholder="Login" 
+                            value={login}
+                            onChange={evento => setLogin(evento.target.value)}
+                        />
                     </div>
                     <div className="input">
                         <img src="/lock.svg" alt="Informe sua senha" />
-                        <input type="password" placeholder="Senha" />
+                        <input type="password" placeholder="Senha" 
+                            value={password}
+                            onChange={evento => setPassword(evento.target.value)}
+                        />
                     </div>
-                    <button>Login</button>
+                    <button onClick={doLogin}>Login</button>
                 </div>
             </form>
         </div>
