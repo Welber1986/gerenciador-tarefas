2e4f1501fcd252c9451503fbd7a37f6eec835f5d
Welber <wlbernardino@gmail.com>
2022-08-06 16:00:31 -0300

    Aula 06/08/2022

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -7,7 +7,22 @@ export const Footer = () => {
                 <span>Tarefas</span>
                 <img src='/filter.svg' alt="Filtrar Tarefas" />
                 <div className="form">
-                    
+                    <div>
+                        <label>Data prevista de conclusão:</label>
+                        <input type='date'/>
+                    </div>
+                    <div>
+                        <label>até:</label>
+                        <input type='date'/>
+                    </div>
+                    <div>
+                        <label>Status:</label>
+                        <select>
+                            <option>Todas</option>
+                            <option>Ativas</option>
+                            <option>Concluídas</option>
+                        </select>
+                    </div>
                 </div>
             </div>
         </div>
